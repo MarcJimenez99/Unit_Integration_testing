2151cd1c95a8271564a9de0e9cd2d18d25d507e2
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-16 16:09:52 -0800

    Updated array.sh and c-echo.h

--- c-echo.h
+++ c-echo.h
@@ -1,9 +1,13 @@
 #include <iostream>
 
-int main(int argv, char** argc) {
-
-	for(int i = 1; i < argv; i++) {
-		std::cout << argc[i] << " ";
+std::string echo(int length, char** chars) {
+	std::string ret = "";
+	for(int i = 1; i < length; i++) {
+		ret += chars[i];
+		if(i < length - 1) {
+			ret += "";
+		}
 	}
-	std::cout << std::endl;
+	ret += "\n";
+	return ret;
 }
