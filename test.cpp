b603dbc9f401b49809eab27cac6c2f0a077fd4f9
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-17 13:08:21 -0800

    Made the rest of the test cases

--- test.cpp
+++ test.cpp
@@ -7,6 +7,23 @@ TEST(EchoTest, HelloWorld) {
 	EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char* test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, ThreeWords) {
+	char* test_val[0] = "./c-echo"; test_val[1] = "What's"; test_val[2] = "Up"; test_val[3] = "Bitches"; 
+	EXPECT_EQ("What's Up Bitches", echo(4, test_val));
+}
+TEST(EchoTest, SpecialCharacters) {
+	char* test_val[0] = "./c-echo"; test_val[1] = "Me"; test_val[2] = "+"; test_val[3] = "Food";  test_val[4] = "="; test_val[5] = "Love";
+	EXPECT_EQ("Me + Food = Love", echo(5,test_val));
+}
+TEST(EchoTest, SpaceLiteral) {
+	char* test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = " "; test_val[3] = "world";
+	EXPECT_EQ("hello   world", echo(4,test_val));
+}
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
